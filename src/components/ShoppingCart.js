2e9c80181920ebc1b330b5865aa94dec562e49ef
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-01-20 17:32:49 -0500

    added the add item function functionality , created PrudctContext and CartContext and consumed the data through them into components"

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,27 +1,31 @@
-import React from 'react';
-
+import React, { useContext } from "react";
+import CartContext from "./../contexts/CartContexts";
 // Components
-import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
-	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
-			return acc + value.price;
-		}, 0).toFixed(2);
-	};
+import Item from "./ShoppingCartItem";
 
-	return (
-		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
-			))}
+const ShoppingCart = (props) => {
+  const cart = useContext(CartContext);
+  const getCartTotal = () => {
+    return cart
+      .reduce((acc, value) => {
+        return acc + value.price;
+      }, 0)
+      .toFixed(2);
+  };
 
-			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
-				<button>Checkout</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="shopping-cart">
+      {cart.map((item) => (
+        <Item key={item.id} {...item} />
+      ))}
+
+      <div className="shopping-cart__checkout">
+        <p>Total: ${getCartTotal()}</p>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingCart;
